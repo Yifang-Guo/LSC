d1926f894eda2350de6fb08e2ff8e3910ef573e9
Yifang-Guo <charlizeguo@gmail.com>
2024-01-03 22:55:08 +0000

    original project

---
+++ src/main/java/com/fangit/LscApplication.java
@@ -0,0 +1,13 @@
+package com.fangit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LscApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LscApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fangit/LscApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fangit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LscApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
